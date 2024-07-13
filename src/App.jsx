78a0849b7659bfe8fdfa1dbb498cc0588aaed3db
moktadir-mirul <moktadir.mirul@gmail.com>
2024-07-13 17:21:23 +0600

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const langName = ['C++', 'PHP', 'Java', 'JavaScript', 'Python'];
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          The Count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+        <h2>The name of the languages are-</h2>
+        <ul>
+          {langName.map((lang) => 
+          <li key={lang}>{lang}</li>
+          )}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
